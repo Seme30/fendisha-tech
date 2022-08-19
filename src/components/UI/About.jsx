95e54758f79ba4f6e49bb18ad6fda382d5ed9c37
Semahegn <83661382+Seme30@users.noreply.github.com>
2022-08-19 20:47:42 +0300

    about-comp

---
+++ src/components/UI/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../../styles/about.css'
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="container">
+        <div className="about__wrapper">
+          <div className="about__content">
+            <h6 className="subtitle">Why choose us</h6>
+            <h2>Specialist in aviding clients on</h2>
+            <h2 className="highlight"> financial challenges</h2>
+            <p className="description about__content-des">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus quae laborum debitis minus. Facere tenetur nemo nesciunt esse voluptas. Praesentium earum facere assumenda iure dolore necessitatibus voluptatem, mollitia quibusdam?
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
